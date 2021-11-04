1f6d580c0ca362c2130b12196b3c0b46e48d3422
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2021-11-04 17:05:11 +0100

    Product images (#6)
    * WIP
    
    * Fixed comments, added jmx_exporter.yaml
    
    * WIP
    
    * WIP
    
    * Improved myid script to have configurable data directory.
    Added stackable user to docker file and simplified pathes.
    
    * Removed outdated example.
    
    * WIP
    
    * Working locally with opa-operator.
    
    * Update the docker workflow.
    
    * Next GH workflow attempt.
    
    * Added buildArgs to docker workflow.
    
    * Fix typo in docker workflow.
    
    * Deleted non stackable image name from docker workflow.
    
    * Attempt to tag image with product version docker workflow.
    
    * Delete attempt to tag image with product version docker workflow.
    
    * Use matrix to build docker images.
    
    * Restore rust image workflow and create separate workflow für product images.
    
    * Added build_product_images.py
    
    * Update README.adoc
    
    * Added support for multiple build args from conf.py
    
    Kafka images are also tagged with the scala version.
    
    * Fixed error in myid.sh where id was always written to local directory (missing $)
    
    * Add stackable to image names and clean up inline docs.
    
    * Added Apache NiFi.
    
    * Added spark
    
    * Added Trino.
    
    * Added Apache Spark to conf.py
    
    * Added Dockerfile for hadoop.
    
    * Add possibility to build images for multiple products at once.
    
    * Added trino
    
    * switched to java8 for trino
    
    * Added Dockerfile for Hive.
    
    * renamed startup.sh to run-druid and moved into druid package folder ./stackable/run-druid
    
    * Added HBase Dockerfile.
    
    * Fix "}" misplacement in zookeeper.
    
    * Cleaned up druid docker file
    
    * Added prometheus and monitoring docker files
    
    * Added Superset
    
    * Add superset configuration file
    
    * Added prometheus-wrapper.sh
    
    * Added copy_assets.sh
    
    * Switched opa repo to stackable repo.
    
    * Added opa-authorizer jar to kafka docker file
    
    * Set HDFS_BIN_DIR env variable
    
    * Added uid and gid for stackable user.
    
    * WIP
    
    * WIP - improving consistency
    
    * Adapted and tested all images except superset.
    
    Co-authored-by: maltesander <malte.sander.it@gmail.com>
    Co-authored-by: Siegfried Weber <mail@siegfriedweber.net>

---
+++ build_product_images.py
@@ -0,0 +1,116 @@
+#!/bin/env python
+"""
+Build and possibly publish product images. It doesn't login to any registry when publishing
+but it assumes a `docker login` has been performed before.
+
+Usage: build_product_images.py --help
+
+Example:
+
+    build_product_images.py --product zookeeper,kafka -image_version 0.1 --push
+
+This whill build an image for each Apache Zookeeper and APache Kafka version configured in conf.py
+"""
+
+import conf
+import argparse
+import subprocess
+import sys
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Build and publish product images. See conf.py for details regarding product versions.")
+    parser.add_argument("-r", "--registry", help="Image registry to publish to.", default='docker.stackable.tech')
+    parser.add_argument("-p", "--product", help="Product names to build as a comma separated list", type=str)
+    parser.add_argument("-i", "--image_version", help="Image version", required=True)
+    parser.add_argument("-u", "--push", help="Push images", action='store_true')
+    parser.add_argument("-d", "--dry", help="Dry run.", action='store_true')
+    return parser.parse_args()
+
+def build_image_args(version):
+    """
+    Returns a list of --build-arg command line arguments that are used by the
+    docker build command.
+
+    Arguments:
+    - version: Can be a str, in which case it's considered the PRODUCT_VERSION
+                or a dict.
+    """
+    result = []
+
+    if isinstance(version, dict):
+        for k, v in version.items():
+            result.extend(['--build-arg', f'{k.upper()}={v}'])
+    elif isinstance(version, str):
+        result=['--build-arg', f'PRODUCT_VERSION={version}']
+    else:
+        raise ValueError(f'Unsupported version object: {version}')
+
+    return result
+
+def build_image_tags(image_name, image_version, product_version):
+    """
+    Returns a list of --tag command line arguments that are used by the
+    docker build command.
+    """
+    result = []
+
+    if isinstance(product_version, dict):
+        all_versions = "-".join([v for v in product_version.values()])
+        result.extend(['-t', f'{image_name}:{all_versions}-{image_version}'])
+    elif isinstance(product_version, str):
+        result=['-t', f'{image_name}:{product_version}-{image_version}']
+    else:
+        raise ValueError(f'Unsupported version object: {product_version}')
+
+    return result
+
+def build_and_publish_image(args, products):
+    """
+    Returns a list of commands that need to be run in order to build and
+    publish product images.
+    """
+    commands = []
+
+    for p in products:
+        for v in p['versions']:
+            image_name=f'{args.registry}/stackable/{p["name"]}'
+            tags = build_image_tags(image_name, args.image_version, v)
+            build_args = build_image_args(v)
+
+            commands.append(['docker', 'build', *build_args, *tags, p["name"]])
+            if args.push:
+                commands.append(['docker', 'push', '--all-tags', image_name])
+        
+    return commands
+
+def run_commands(dry, commands):
+    """
+    Runs the commands to build and publish images. In dry-run mode it only
+    lists the command on stdout.
+    """
+    for cmd in commands:
+        if dry:
+            subprocess.run(['echo', *cmd])
+        else:
+            ret = subprocess.run(cmd)
+            if ret.returncode != 0:
+                sys.exit(1)
+
+def products_to_build(product_names, products):
+    if not product_names:
+        return products
+    else:
+        pnd = {n: 1 for n in product_names.split(',')}
+        return list(filter(lambda p: p['name'] in pnd, products))
+
+def main():
+    args = parse_args()
+
+    products = products_to_build(args.product, conf.products)
+
+    commands = build_and_publish_image(args, products)
+
+    run_commands(args.dry, commands)
+
+if __name__ == "__main__":
+    main()
---
+++ conf.py
@@ -0,0 +1,73 @@
+"""
+Application images will be created for products and associated versions configured here.
+"""
+products = [
+    {
+        'name': 'druid',
+        'versions': ['0.22.0'],
+    },
+    {
+        'name': 'hadoop',
+        'versions': ['3.2.2', '3.3.1'],
+    },
+    {
+        'name': 'hbase',
+        'versions': ['2.4.6'],
+    },
+    {
+        'name': 'hive',
+        'versions': ['2.3.9'],
+    },
+    {
+        # Opa authorizer 1.1.0 for Kafka version < 3.0.0
+        # Opa authorizer 1.2.0 for Kafka version >= 3.0.0
+        'name': 'kafka',
+        'versions': [
+            {
+                'product_version': '2.8.0',
+                'scala_version': '2.12',
+                'opa_authorizer_version': '1.1.0',
+            },
+            {
+                'product_version': '2.8.0',
+                'scala_version': '2.13',
+                'opa_authorizer_version': '1.1.0',
+            },
+        ]
+    },
+    {
+        'name': 'nifi',
+        'versions': ['1.13.2'],
+    },
+    {
+        'name': 'opa',
+        'versions': ['0.27.1'],
+    },
+    {
+        'name': 'spark',
+        'versions': [
+            {
+                'product_version': '3.0.1',
+                'hadoop_version': '2.7',
+            },
+            {
+                'product_version': '3.1.1',
+                'hadoop_version': '2.7',
+            },
+        ]
+    },
+    {
+        'name': 'superset',
+        'versions': ['1.3.2'],
+    },
+    {
+        'name': 'trino',
+        'versions': ['362'],
+    },
+    {
+        # ZooKeeper PRODUCT_VERSION must be at least 3.5.0 in order to work
+        'name': 'zookeeper',
+        'versions': ['3.5.8', '3.7.0'],
+    },
+ ]
+
