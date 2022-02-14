1f0b2e135e6d7119546834f4a223efd9b526ec76
Siegfried Weber <mail@siegfriedweber.net>
2022-02-14 09:40:08 +0100

    Enable statsd logging in Superset

--- superset/stackable/superset_config.py
+++ superset/stackable/superset_config.py
@@ -1,4 +1,6 @@
 import os
+from superset.stats_logger import StatsdStatsLogger
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+STATS_LOGGER = StatsdStatsLogger(host='0.0.0.0', port=9125)
