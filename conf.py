7c0302cc27df657f3230c63f8a5526bbc4563f2a
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2022-03-04 13:55:54 +0100

    Use OPA Kafka plugin 1.4.0 … (#61)
    * Switch from 1.1.0 to 1.4.0 of the OPA Kafka plugin for most Kafka versions.
    
    * Update PR version
    
    * Remove image for kafka 2.6.2

--- conf.py
+++ conf.py
@@ -19,24 +19,17 @@ products = [
         'versions': ['2.3.9'],
     },
     {
-        # Opa authorizer 1.1.0 for Kafka version < 3.0.0
-        # Opa authorizer 1.4.0 for Kafka version >= 3.0.0
         'name': 'kafka',
         'versions': [
             {
-                'product': '2.6.2',
-                'scala': '2.13',
-                'opa_authorizer': '1.1.0',
-            },
-            {
                 'product': '2.7.1',
                 'scala': '2.13',
-                'opa_authorizer': '1.1.0',
+                'opa_authorizer': '1.4.0',
             },
             {
                 'product': '2.8.1',
                 'scala': '2.13',
-                'opa_authorizer': '1.1.0',
+                'opa_authorizer': '1.4.0',
             },
             {
                 'product': '3.1.0',
