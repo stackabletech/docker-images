42fc9388a5e3d807f71d4cd3cf54e178f66b90cb
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-03-18 11:42:31 +0100

    Spark k8s init (#73)
    * spark-k8s: initial image
    
    * spark-k8s: pass in tini executable rather than downloading it, added changelog/deps
    
    * spark-k8s: consolidate RUN commands
    
    * removed python components (will be attached to job image)

--- conf.py
+++ conf.py
@@ -64,6 +64,15 @@ products = [
         ]
     },
     {
+        'name': 'spark-k8s',
+        'versions': [
+            {
+                'product': '3.2.1',
+                'hadoop': '3.2',
+            },
+        ]
+    },
+    {
         'name': 'superset',
         'versions': [
             {
@@ -99,4 +108,3 @@ products = [
         'versions': ['3.5.8', '3.6.3', '3.7.0'],
     },
  ]
-
