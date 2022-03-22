85412eab383dc5ab030257be82d126c26ec63932
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2022-03-22 14:59:30 +0100

    Spark k8s aws dependendency (#78)
    * spark-k8s: added aws dependencies in image
    
    * Cleanup and update CHANGELOG
    
    Co-authored-by: Andrew Kenworthy <andrew.kenworthy@stackable.de>

--- conf.py
+++ conf.py
@@ -88,6 +88,7 @@ products = [
                 'product': '3.2.1',
                 'hadoop': '3.2',
                 'python': '39',
+                'aws': '1.11.375',
             },
         ]
     },
