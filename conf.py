82f331fcb85f7315ac51003a6d8ef7b3aa69fb1e
Malte Sander <malte.sander.it@gmail.com>
2022-03-29 09:37:35 +0200

    java-base image for 1.8.0 and 11 (#81)
    * java-base image for 8 and 11
    
    * fixed flake
    
    * tagged ubi-minimal base image

--- conf.py
+++ conf.py
@@ -41,6 +41,19 @@ products = [
         'versions': ['2.3.9'],
     },
     {
+        'name': 'java-base',
+        'versions': [
+            {
+                'product': '1.8.0',
+                '_security_path': '/usr/lib/jvm/jre-1.8.0/lib/security/java.security',
+            },
+            {
+                'product': '11',
+                '_security_path': '/usr/lib/jvm/jre-11/conf/security/java.security',
+            },
+        ],
+    },
+    {
         'name': 'kafka',
         'versions': [
             {
