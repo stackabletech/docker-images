5e4b7b17538e820883c59e3f848ee44133a7c227
Maxi Wittich <maximilian.wittich@stackable.de>
2022-08-09 12:39:59 +0200

    Repository path is now optional

--- build_product_images.py
+++ build_product_images.py
@@ -60,6 +60,11 @@ def parse_args():
         required=True,
         type=check_architecture_input
     )
+    parser.add_argument(
+        "--path",
+        help="Define a custom location or repository",
+        default="stackable"
+    )
     return parser.parse_args()
 
 
@@ -109,7 +114,7 @@ def build_and_publish_image(args, product) -> List[List[str]]:
 
     For local building, builder instances are supported.
     """
-    image_name = f'{args.registry}/stackable/{product["name"]}'
+    image_name = f'{args.registry}/{args.path}/{product["name"]}'
     tags = build_image_tags(image_name, args.image_version, args.product_version)
     build_args = build_image_args(product["versions"][0])
 
