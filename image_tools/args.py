6003793a992013422040852531260a1aade9ba57
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-03-06 13:20:30 +0100

    Refactor image building and add preflight checks. (#339)
    * Refactor image building and add preflight checks.
    
    * Update changelog
    
    * Fix lint warnings.
    
    * Fix more lint warnings.
    
    * A bit of cleanup.

---
+++ image_tools/args.py
@@ -0,0 +1,74 @@
+# This is the stackable release version
+from argparse import Namespace, ArgumentParser
+from typing import List
+import re
+
+DEFAULT_IMAGE_VERSION_FORMATS = [
+    re.compile(r"[2-9][0-9]\.[1-9][0-2]?\.\d+"),
+    re.compile(r"[2-9][0-9]\.[1-9][0-2]?\.\d+-rc[1-9]\d?"),
+]
+
+
+def parse() -> Namespace:
+    parser = ArgumentParser(
+        description="Build and publish product images. Requires docker and buildx (https://github.com/docker/buildx)."
+    )
+    parser.add_argument("-i", "--image-version", help="Image version", required=True, type=check_image_version_format)
+    parser.add_argument("-p", "--product", help="Product to build images for")
+    parser.add_argument("-u", "--push", help="Push images", action="store_true")
+    parser.add_argument("-d", "--dry", help="Dry run.", action="store_true")
+    parser.add_argument(
+        "-a",
+        "--architecture",
+        help="Target platform for image. Default: linux/amd64.",
+        nargs="+",
+        default=["linux/amd64"],
+        type=check_architecture_input,
+    )
+    parser.add_argument(
+        "-o",
+        "--organization",
+        help="Organization name within the given registry. Default: stackable",
+        default="stackable",
+    )
+    parser.add_argument(
+        "-r",
+        "--registry",
+        help="Image registry to publish to. Default: docker.stackable.tech",
+        default="docker.stackable.tech",
+    )
+    return parser.parse_args()
+
+
+def check_image_version_format(image_version) -> str:
+    """
+    Check image version against allowed formats.
+
+    >>> check_image_version_format("23.4.0")
+    '23.4.0'
+    >>> check_image_version_format("23.4.0-rc1")
+    '23.4.0-rc1'
+    >>> check_image_version_format("23.04.0")
+    Traceback (most recent call last):
+    ...
+    ValueError: Invalid image version: 23.04.0
+    >>> check_image_version_format("23.4.0.prerelease")
+    Traceback (most recent call last):
+    ...
+    ValueError: Invalid image version: 23.4.0.prerelease
+    """
+    for p in DEFAULT_IMAGE_VERSION_FORMATS:
+        if p.fullmatch(image_version):
+            return image_version
+    raise ValueError(f"Invalid image version: {image_version}")
+
+
+def check_architecture_input(architecture) -> List[str]:
+    supported_arch = ["linux/amd64", "linux/arm64"]
+
+    if architecture not in supported_arch:
+        raise ValueError(
+            f"Architecture {architecture} not supported. Supported: {supported_arch}"
+        )
+
+    return architecture
---
+++ image_tools/bake.py
@@ -0,0 +1,154 @@
+"""Image builder
+
+Requirements: docker and buildx.
+
+Usage:
+
+    python -m image_tools.bake -p opa -i 22.12.0
+"""
+from image_tools.lib import Command
+import image_tools.conf as conf
+from image_tools.args import parse
+from typing import List, Dict, Any
+from argparse import Namespace
+from subprocess import run
+import json
+
+
+def build_image_args(version, release_version):
+    """
+    Returns a list of --build-arg command line arguments that are used by the
+    docker build command.
+
+    Arguments:
+    - version: Can be a str, in which case it's considered the PRODUCT
+                or a dict.
+    """
+    result = {}
+
+    if isinstance(version, dict):
+        for k, v in version.items():
+            result[k.upper()] = v
+        result["RELEASE"] = release_version
+    elif isinstance(version, str) and isinstance(release_version, str):
+        {
+            "PRODUCT": version,
+            "RELEASE": release_version,
+        }
+    else:
+        raise ValueError(f"Unsupported version object: {version}")
+
+    return result
+
+
+def build_image_tags(image_name: str, image_version: str, product_version: str) -> List[str]:
+    """
+    Returns the --tag command line arguments that are used by the docker build command.
+    """
+    return [
+        f"{image_name}:{product_version}-stackable{image_version}",
+    ]
+
+
+def generate_bakefile(args: Namespace) -> Dict[str, Any]:
+    """
+    Generates a Bakefile (https://docs.docker.com/build/bake/file-definition/) describing how to build the whole image graph.
+
+    build_and_publish_images() ensures that only the desired images are actually built.
+    """
+    targets = {}
+    groups = {}
+    product_names = [product["name"] for product in conf.products]
+    for product in conf.products:
+        product_name = product["name"]
+        product_targets = {}
+        for version_dict in product.get("versions"):
+            product_targets.update(bakefile_product_version_targets(args, product_name, version_dict, product_names))
+        groups[product_name] = {
+            "targets": list(product_targets.keys()),
+        }
+        targets.update(product_targets)
+    groups["default"] = {
+        "targets": list(groups.keys()),
+    }
+    return {
+        "target": targets,
+        "group": groups,
+    }
+
+
+def bakefile_target_name_for_product_version(product_name: str, version: str) -> str:
+    """
+    Creates a normalized Bakefile target name for a given (product, version) combination.
+    """
+    return f"{ product_name }-{ version.replace('.', '_') }"
+
+
+def bakefile_product_version_targets(args: Namespace, product_name: str, versions: Dict[str, str], product_names: List[str]):
+    """
+    Creates Bakefile targets defining how to build a given product version.
+
+    A product is assumed to depend on another if it defines a `versions` field with the same name as the other product.
+    """
+    image_name = f'{args.registry}/{args.organization}/{product_name}'
+    tags = build_image_tags(
+        image_name, args.image_version, versions["product"]
+    )
+    build_args = build_image_args(versions, args.image_version)
+
+    return {
+        bakefile_target_name_for_product_version(product_name, versions['product']): {
+            "dockerfile": f"{ product_name }/Dockerfile",
+            "tags": tags,
+            "args": build_args,
+            "platforms": args.architecture,
+            "context": ".",
+            "contexts": {f"stackable/image/{dep_name}": f"target:{bakefile_target_name_for_product_version(dep_name, dep_version)}" for dep_name, dep_version in versions.items() if dep_name in product_names},
+        },
+    }
+
+
+def bake_command(args: Namespace, product_name: str, bakefile) -> Command:
+    """
+    Returns a list of commands that need to be run in order to build and
+    publish product images.
+
+    For local building, builder instances are supported.
+    """
+
+    if args.push:
+        target_mode = ["--push"]
+    elif len(args.architecture) == 1:
+        target_mode = ["--load"]
+    else:
+        target_mode = []
+
+    return Command(
+        args=[
+            "docker",
+            "buildx",
+            "bake",
+            "--file",
+            "-",
+            *([] if product_name is None else [product_name]),
+            *target_mode,
+        ],
+        stdin=json.dumps(bakefile),
+    )
+
+
+def main():
+    """Generate a Docker bake file from conf.py and build the given args.product images."""
+    args = parse()
+    bakefile = generate_bakefile(args)
+
+    cmd = bake_command(args, args.product, bakefile)
+
+    if args.dry:
+        print(cmd)
+    else:
+        run(cmd.args, input=cmd.input, check=True)
+
+
+if __name__ == '__main__':
+    main()
