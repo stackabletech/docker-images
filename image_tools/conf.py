71c685f8ff579aacbb72e9396a7b76e29e55b52f
Malte Sander <malte.sander.it@gmail.com>
2023-05-26 14:54:50 +0200

    Support OPA 0.51.0 (#382)
    * add opa 0.51.0 and make bundle builder version configurable
    
    * enable all versions
    
    * adapted changelog
    
    * Update opa/Dockerfile
    
    Co-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.de>
    
    * fix bundle builder arg
    
    ---------
    
    Co-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.de>

--- image_tools/conf.py
+++ image_tools/conf.py
@@ -211,22 +211,32 @@ products = [
             {
                 "product": "0.27.1",
                 "vector": "0.26.0",
+                "bundle_builder_version": "1.0.0",
             },
             {
                 "product": "0.28.0",
                 "vector": "0.26.0",
+                "bundle_builder_version": "1.0.0",
             },
             {
                 "product": "0.37.2",
                 "vector": "0.26.0",
+                "bundle_builder_version": "1.0.0",
             },
             {
                 "product": "0.41.0",
                 "vector": "0.26.0",
+                "bundle_builder_version": "1.0.0",
             },
             {
                 "product": "0.45.0",
                 "vector": "0.26.0",
+                "bundle_builder_version": "1.0.0",
+            },
+            {
+                "product": "0.51.0",
+                "vector": "0.26.0",
+                "bundle_builder_version": "1.1.0",
             },
         ],
     },
