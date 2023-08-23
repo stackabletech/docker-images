716bcafa0ee6c443468b948d2eec900ab2683192
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-08-23 17:18:24 +0200

    Extract image-tools to it's own repository.

--- conf.py
+++ conf.py
@@ -1,4 +1,6 @@
 """
+Configuration file for the Stackable image-tools: https://github.com/stackabletech/image-tools.
+
 Application images will be created for products and associated versions configured here.
 """
 
