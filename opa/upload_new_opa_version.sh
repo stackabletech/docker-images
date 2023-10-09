94805bdb3986d7b89fe5bdfc46d2c8b30701dca7
Malte Sander <malte.sander.it@gmail.com>
2023-10-09 17:51:13 +0200

    use original opa bins instead of packing into tar archive

--- opa/upload_new_opa_version.sh
+++ opa/upload_new_opa_version.sh
@@ -29,40 +29,29 @@ trap cleanup EXIT
 
 cd "$WORK_DIR" || exit
 
-downloaded_bin_file=opa_linux_amd64_static
-download_url="https://openpolicyagent.org/downloads/v${VERSION}/${downloaded_bin_file}"
-bin_file=opa
-bin_file_zipped="${bin_file}"_"${VERSION}".tar.gz
+bin_file=opa_linux_amd64_static
+download_url="https://openpolicyagent.org/downloads/v${VERSION}/${bin_file}"
 
-# We need to keep the original bin file name for now to have the easy diff on the checksum working
-# Will rename it to just "opa" later
 echo "Downloading OPA from ${download_url}"
-curl --fail -L -o "${downloaded_bin_file}" "${download_url}"
+curl --fail -L -o "${bin_file}" "${download_url}"
 echo "Downloading OPA checksum from ${download_url}.sha256"
-curl --fail -L -o "${bin_file_zipped}".sha256 "${download_url}.sha256"
+curl --fail -L -o "${bin_file}".sha256 "${download_url}".sha256
 
 echo "Validating SHA256 Checksum"
-if ! (sha256sum "${downloaded_bin_file}" | diff - "${bin_file_zipped}".sha256); then
+if ! (sha256sum "${bin_file}" | diff - "${bin_file}".sha256); then
   echo "ERROR: One of the SHA256 sums does not match"
   exit 1
 fi
 
-echo "Rename downloaded bin file ${downloaded_bin_file} to ${bin_file}"
-mv ${downloaded_bin_file} ${bin_file}
-
-echo "Set execute permissions on bin file ${bin_file}"
-chmod 755 "${bin_file}"
-
-echo "Compressing ${bin_file} bin file to ${bin_file_zipped}"
-tar -czf "${bin_file_zipped}" "${bin_file}"
-
-echo "Recreating SHA512 checksum for compressed bin file ${bin_file_zipped}"
-sha512sum "${bin_file_zipped}" > "${bin_file_zipped}".sha512
+versioned_bin_file=${bin_file}_${VERSION}
+echo "Tag bin file and SHA with version ${bin_file} -> ${versioned_bin_file}"
+mv "${bin_file}" "${versioned_bin_file}"
+mv "${bin_file}".sha256 "${versioned_bin_file}".sha256
 
 echo "Uploading everything to Nexus"
 EXIT_STATUS=0
-curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "${bin_file_zipped}" 'https://repo.stackable.tech/repository/packages/opa/' || EXIT_STATUS=$?
-curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "${bin_file_zipped}".sha512 'https://repo.stackable.tech/repository/packages/opa/' || EXIT_STATUS=$?
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "${versioned_bin_file}" 'https://repo.stackable.tech/repository/packages/opa/' || EXIT_STATUS=$?
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "${versioned_bin_file}".sha256 'https://repo.stackable.tech/repository/packages/opa/' || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then
   echo "ERROR: Upload failed"
