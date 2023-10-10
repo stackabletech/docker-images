0aecea1118c248ee47c8c036cc48ddf9d76cee2b
Malte Sander <malte.sander.it@gmail.com>
2023-10-10 13:06:22 +0200

    adapt versions

--- conf.py
+++ conf.py
@@ -252,22 +252,22 @@ products = [
     {
         "name": "spark-k8s",
         "versions": [
-            {
-                "product": "3.4.0",
-                "spark": "3.4.0",
-                "java-base": "11",
-                "python": "3.11",
-                "hadoop_short_version": "3",
-                "hadoop_long_version": "3.3.4",
-                "aws_java_sdk_bundle": "1.12.262",
-                "azure_storage": "7.0.1",
-                "azure_keyvault_core": "1.0.0",
-                "jackson_dataformat_xml": "2.14.2",
-                "stax2_api": "4.2.1",
-                "woodstox_core": "6.5.0",
-                "vector": "0.31.0",
-                "jmx_exporter": "0.20.0",
-            },
+            # {
+            #     "product": "3.4.0",
+            #     "spark": "3.4.0",
+            #     "java-base": "11",
+            #     "python": "3.11",
+            #     "hadoop_short_version": "3",
+            #     "hadoop_long_version": "3.3.4",
+            #     "aws_java_sdk_bundle": "1.12.262",
+            #     "azure_storage": "7.0.1",
+            #     "azure_keyvault_core": "1.0.0",
+            #     "jackson_dataformat_xml": "2.14.2",
+            #     "stax2_api": "4.2.1",
+            #     "woodstox_core": "6.5.0",
+            #     "vector": "0.31.0",
+            #     "jmx_exporter": "0.20.0",
+            # },
             {
                 "product": "3.4.1",
                 "spark": "3.4.1",
@@ -284,22 +284,22 @@ products = [
                 "vector": "0.31.0",
                 "jmx_exporter": "0.20.0",
             },
-            {
-                "product": "3.5.0",
-                "spark": "3.5.0",
-                "java-base": "11",
-                "python": "3.11",
-                "hadoop_short_version": "3",
-                "hadoop_long_version": "3.3.4",  # https://github.com/apache/spark/blob/1db2f5c36b120c213432fc658c9fd24fc73cb45e/pom.xml#L122
-                "aws_java_sdk_bundle": "1.12.262",  # https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-aws/3.3.4
-                "azure_storage": "7.0.1",  # https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure/3.3.4
-                "azure_keyvault_core": "1.0.0",  # https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage/7.0.1
-                "jackson_dataformat_xml": "2.14.2",  # https://mvnrepository.com/artifact/org.apache.spark/spark-core_2.13/3.4.0
-                "stax2_api": "4.2.1",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.14.2
-                "woodstox_core": "6.5.0",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.14.2
-                "vector": "0.31.0",
-                "jmx_exporter": "0.20.0",
-            },
+            # {
+            #     "product": "3.5.0",
+            #     "spark": "3.5.0",
+            #     "java-base": "11",
+            #     "python": "3.11",
+            #     "hadoop_short_version": "3",
+            #     "hadoop_long_version": "3.3.4",  # https://github.com/apache/spark/blob/1db2f5c36b120c213432fc658c9fd24fc73cb45e/pom.xml#L122
+            #     "aws_java_sdk_bundle": "1.12.262",  # https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-aws/3.3.4
+            #     "azure_storage": "7.0.1",  # https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure/3.3.4
+            #     "azure_keyvault_core": "1.0.0",  # https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage/7.0.1
+            #     "jackson_dataformat_xml": "2.14.2",  # https://mvnrepository.com/artifact/org.apache.spark/spark-core_2.13/3.4.0
+            #     "stax2_api": "4.2.1",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.14.2
+            #     "woodstox_core": "6.5.0",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.14.2
+            #     "vector": "0.31.0",
+            #     "jmx_exporter": "0.20.0",
+            # },
         ],
     },
     {
