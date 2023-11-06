6a6d2091ac4ba501880630b5ab89af258371b62d
Lars Francke <git@lars-francke.de>
2023-11-06 10:13:48 +0100

    HBase 2.4.12 builds as well now

--- hbase/stackable/patches/apply_patches.sh
+++ hbase/stackable/patches/apply_patches.sh
@@ -4,21 +4,40 @@
 set -eu
 set -o pipefail
 
-# Check if $1 (PRODUCT) is provided
+# Check if $1 (VERSION) is provided
 if [ -z "${1-}" ]; then
-  echo "Please provide a value for PRODUCT as the first argument."
+  echo "Please provide a value for VERSION as the first argument."
   exit 1
 fi
 
-PRODUCT="$1"
+VERSION="$1"
+PATCH_DIR="patches/$VERSION"
 
-# Iterate through patch files in the PATCH_DIR
-# TODO: Make it more deterministic, patch files should have a sequence number in their name to apply them in order
-for patch_file in patches/*; do
-  echo "Checking $patch_file"
-  # Check if the patch file matches the PRODUCT - this could probably also be more robust
-  if [[ $patch_file == *"$PRODUCT"* ]]; then
-    echo "Applying $patch_file"
-    git apply --directory "hbase-${PRODUCT}-src" "$patch_file"
-  fi
+# Check if version-specific patches directory exists
+if [ ! -d "$PATCH_DIR" ]; then
+  echo "Patches directory '$PATCH_DIR' does not exist."
+  exit 1
+fi
+
+# Create an array to hold the patches in sorted order
+declare -a patch_files
+
+echo "Applying patches from ${PATCH_DIR}" now
+
+# Read the patch files into the array
+while IFS= read -r -d $'\0' file; do
+  patch_files+=("$file")
+done < <(find "$PATCH_DIR" -name "*.patch" -print0 | sort -zV)
+
+echo "Found ${#patch_files[@]} patches, applying now"
+
+# Iterate through sorted patch files
+for patch_file in "${patch_files[@]}"; do
+  echo "Applying $patch_file"
+  git apply --directory "hbase-${VERSION}-src" "$patch_file" || {
+    echo "Failed to apply $patch_file"
+    exit 1
+  }
 done
+
+echo "All patches applied successfully."
