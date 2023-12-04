2bad8bffec125d59aa8ea9ceacaa3f866b1bb95b
Natalie Klestrup Röijezon <nat@nullable.se>
2023-12-04 10:25:05 +0100

    Parametrize JMX exporter version

--- conf.py
+++ conf.py
@@ -226,6 +226,7 @@ products = [
             {
                 "product": "1.1.0",
                 "java-base": "11",
+                "jmx_exporter": "0.20.0",
             },
         ],
     },
