3719622df39b70bae86c760073e6951219fd3372
Nick Larsen <nick.larsen@stackable.tech>
2024-03-13 13:53:09 +0100

    fix(airflow): shellcheck lints

--- airflow/download_constraints.sh
+++ airflow/download_constraints.sh
@@ -8,7 +8,7 @@ FILENAME="constraints-${AIRFLOW_VERSION}-python${PYTHON_VERSION//.}.txt"
 
 # Find the directory name of the script so it can be run from outside
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd ${DIR} > /dev/null || exit 1
+pushd "${DIR}" > /dev/null || exit 1
 
 echo "Downloading constraints file for Airflow ${AIRFLOW_VERSION} (Python ${PYTHON_VERSION})"
 curl --fail -Ls "${URL}" -o "${FILENAME}"
