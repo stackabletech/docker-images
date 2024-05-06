e72efbcb768584f8f1e6291cd4074d6457efd36d
Lars Francke <git@lars-francke.de>
2024-05-06 12:30:21 +0200

    Use a shared copy_artifacts.sh script

--- ubi9-rust-builder/copy_artifacts.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Copy over the binary
-cp "$1" /app
-
-# And now try to find a BOM file named like the binary + .cdx.xml and copy it over as well if it exists
-base=$(basename "$1")
-find /src/rust/ -type f -name "$base.cdx.xml" -exec cp {} /app \;
