4bffa561d6e34686019953fa4343440227d53138
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2024-05-07 10:56:03 +0200

    Build Kafka from source

--- kafka/upload_new_kafka_version.sh
+++ kafka/upload_new_kafka_version.sh
@@ -29,38 +29,47 @@ trap cleanup EXIT
 
 cd "$WORK_DIR" || exit
 
-bin_file="kafka_2.13-${VERSION}.tgz"
+bin_file=kafka_2.13-$VERSION.tgz
+src_file=kafka-$VERSION-src.tgz
 
 echo "Downloading Kafka (this can take a while, it is intentionally downloading from a slow mirror that contains all old versions)"
-curl --fail -LOs "https://downloads.apache.org/kafka/${VERSION}/${bin_file}"
-curl --fail -LOs "https://downloads.apache.org/kafka/${VERSION}/${bin_file}.asc"
-curl --fail -LOs "https://downloads.apache.org/kafka/${VERSION}/${bin_file}.sha512"
+curl --fail -LOs "https://archive.apache.org/dist/kafka/$VERSION/$bin_file"
+curl --fail -LOs "https://archive.apache.org/dist/kafka/$VERSION/$bin_file.asc"
+curl --fail -LOs "https://archive.apache.org/dist/kafka/$VERSION/$bin_file.sha512"
+
+curl --fail -LOs "https://archive.apache.org/dist/kafka/$VERSION/$src_file"
+curl --fail -LOs "https://archive.apache.org/dist/kafka/$VERSION/$src_file.asc"
+curl --fail -LOs "https://archive.apache.org/dist/kafka/$VERSION/$src_file.sha512"
 
 # It is probably redundant to check both the checksum and the signature but it's cheap and why not
 echo "Validating SHA512 Checksum"
-if ! (gpg --print-md SHA512 "${bin_file}" | diff - "${bin_file}.sha512" ); then
+if ! (gpg --print-md SHA512 "$bin_file" | diff - "$bin_file.sha512" && gpg --print-md SHA512 "$src_file" | diff - "$src_file.sha512" ); then
   echo "ERROR: The SHA512 sum does not match"
   exit 1
 fi
 
-echo "Validating signature"
+echo "Validating signatures"
 echo '--> NOTE: Make sure you have downloaded and added the KEYS file (https://downloads.apache.org/kafka/KEYS) to GPG: https://www.apache.org/info/verification.html (e.g. by using "curl https://downloads.apache.org/kafka/KEYS | gpg --import")'
 
-if ! (gpg --verify "${bin_file}.asc" "${bin_file}" 2> /dev/null); then
-  echo "ERROR: The signature could not be verified"
+if ! (gpg --verify "$bin_file.asc" "$bin_file" 2> /dev/null && gpg --verify "$src_file.asc" "$src_file" 2> /dev/null); then
+  echo "ERROR: One of the signatures could not be verified"
   exit 1
 fi
 
 echo "Uploading everything to Nexus"
 EXIT_STATUS=0
-curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "${bin_file}" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
-curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "${bin_file}.asc" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
-curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "${bin_file}.sha512" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "$bin_file" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "$bin_file.asc" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "$bin_file.sha512" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
+
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "$src_file" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "$src_file.asc" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
+curl --fail -u "$NEXUS_USER:$NEXUS_PASSWORD" --upload-file "$src_file.sha512" 'https://repo.stackable.tech/repository/packages/kafka/' || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then
   echo "ERROR: Upload failed"
   exit 1
 fi
 
-echo "Successfully uploaded version ${VERSION} of Kafka to Nexus"
+echo "Successfully uploaded version $VERSION of Kafka to Nexus"
 echo "https://repo.stackable.tech/service/rest/repository/browse/packages/kafka/"
