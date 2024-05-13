fe706efc07cc6b2d75ad2967d131507c3581966c
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-05-13 17:24:10 +0200

    update conf.py

--- conf.py
+++ conf.py
@@ -183,9 +183,11 @@ products = [
         "versions": [
             {
                 "product": "1.8.0",
+                "stackable-base": "1.0.0",
             },
             {
                 "product": "11",
+                "stackable-base": "1.0.0",
             },
         ],
     },
