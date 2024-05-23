3082d37d8ce1bb1dd096628fb51e152c1823fffd
Maxi Wittich <maximilian.wittich@stackable.tech>
2024-05-23 13:02:41 +0200

    Adding comments

--- upload_sig_storage_driver.sh
+++ upload_sig_storage_driver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 VERSION=${1:?"Missing version number argument (arg 1)"}
 NEXUS_USER=${2:?"Missing Nexus username argument (arg 2)"}
 PRODUCT=${3:?"Missing product argument (arg 3)"}
@@ -13,20 +15,34 @@ ARCHITECTURES=(
 
 function pull_retag_and_push_images() {
     for arch in "${ARCHITECTURES[@]}"; do
-        docker pull "${REGISTRY}/${PRODUCT}:v${VERSION}" --platform linux/${arch}
+        echo "Pulling ${REGISTRY}/${PRODUCT}:v${VERSION} for ${arch}"
+        docker pull "${REGISTRY}/${PRODUCT}:v${VERSION}" --platform "linux/${arch}"
+
+        echo "Retagging image for ${arch}"
         docker tag "${REGISTRY}/${PRODUCT}:v${VERSION}" "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}-${arch}"
+
+        echo "Pushing retagged image for ${arch}"
         docker push "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}-${arch}"
     done
 }
 
 function create_and_push_manifest_list() {
-    docker manifest create "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}" --amend "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}-amd64" --amend "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}-arm64"
+    echo "Creating manifest list"
+    docker manifest create "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}" \
+        --amend "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}-amd64" \
+        --amend "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}-arm64"
+
+    echo "Pushing manifest list"
     LIST_DIGEST=$(docker manifest push "${STACKABLE_REGISTRY}/${PRODUCT}:v${VERSION}")
+
+    echo "Signing manifest list"
     cosign sign -y "${STACKABLE_REGISTRY}/${PRODUCT}@${LIST_DIGEST}"
 }
 
-function docker_login(){
+function docker_login() {
     read -r -s -p "Nexus Password: " NEXUS_PASSWORD
+    echo
+    echo "Logging in to Docker registry"
     docker login -u $NEXUS_USER -p $NEXUS_PASSWORD docker.stackable.tech
 }
 
@@ -37,7 +53,7 @@ function sign_images() {
     done
 }
 
-main() {
+function main() {
     docker_login
     pull_retag_and_push_images
     #sign_images
