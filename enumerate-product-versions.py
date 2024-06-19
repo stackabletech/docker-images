f030a5a21d4979d3498d1268f6b0b1c282818335
Lars Francke <git@lars-francke.de>
2024-06-19 12:28:08 +0200

    Change the dev workflow so that manifest lists are built and pushed right after the images are built
    If needed this can be split out even further to build every product version separately

--- enumerate-product-versions.py
+++ enumerate-product-versions.py
@@ -1,4 +1,5 @@
 import conf
+import sys
 
 PRODUCTS = [
     "airflow",
@@ -21,9 +22,18 @@ PRODUCTS = [
     "zookeeper",
 ]
 
+single_product = None
+if len(sys.argv) > 1:
+    single_product = sys.argv[1]
+    if single_product not in PRODUCTS:
+        print(f"Error: {single_product} is not a valid product.")
+        sys.exit(1)
+
 for product in conf.products:
     product_name = product['name']
     if product_name not in PRODUCTS:
+        continue
+    if single_product and product_name != single_product:
         continue
 
     for version in product['versions']:
