33d5cd558cc52b08106838a3b24e8b91c75d1ef5
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2024-07-03 11:48:28 +0200

    Bump all misc, non-product versions (#737)
    * bumo rust toolchain, ubi & protoc
    
    * bump vector to 0.39.0
    
    * bump vector
    
    * bump jmx exporter to 1.0.1
    
    * bump statsd exporter to 0.26.1
    
    * Bump inotify to 3.22.1.0-1.el9
    
    * Document pushing of k8s/sig-storage images
    
    * bump git sync to 4.2.3
    
    * Add needed cache to conf.py
    
    * changelog
    
    ---------
    
    Co-authored-by: Benedikt Labrenz <benedikt@labrenz.org>

--- opa/versions.py
+++ opa/versions.py
@@ -1,13 +1,13 @@
 versions = [
     {
         "product": "0.57.0",
-        "vector": "0.35.0",
+        "vector": "0.39.0",
         "bundle_builder_version": "1.1.2",
         "stackable-base": "1.0.0",
     },
     {
         "product": "0.61.0",
-        "vector": "0.35.0",
+        "vector": "0.39.0",
         "bundle_builder_version": "1.1.2",
         "stackable-base": "1.0.0",
     },
