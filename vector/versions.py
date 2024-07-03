a45d90606e2cf395f5a1f17403db86d8a000efa2
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2024-07-03 10:30:37 +0200

    Bump inotify to 3.22.1.0-1.el9

--- vector/versions.py
+++ vector/versions.py
@@ -3,6 +3,6 @@ versions = [
         "product": "0.39.0",
         "rpm_release": "1",
         "stackable-base": "1.0.0",
-        "inotify_tools": "3.14-19.el8",
+        "inotify_tools": "3.22.1.0-1.el9",
     }
 ]
