f1dc1ea82a3e66d486025f27fd57e4f865db013a
Benedikt Labrenz <benedikt@labrenz.org>
2024-07-03 16:07:51 +0200

    opa: remove version 0.57.0 and add version 0.66.0

--- opa/versions.py
+++ opa/versions.py
@@ -1,12 +1,12 @@
 versions = [
     {
-        "product": "0.57.0",
+        "product": "0.61.0",
         "vector": "0.39.0",
         "bundle_builder_version": "1.1.2",
         "stackable-base": "1.0.0",
     },
-    {
-        "product": "0.61.0",
+        {
+        "product": "0.66.0",
         "vector": "0.39.0",
         "bundle_builder_version": "1.1.2",
         "stackable-base": "1.0.0",
