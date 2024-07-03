b8b74a5b200faf4c70003a0b31111da9ceacb50e
Maxi Wittich <maximilian.wittich@stackable.tech>
2024-07-03 10:52:28 +0200

    Removing zookeeper 3.8.3 as depricated

--- zookeeper/versions.py
+++ zookeeper/versions.py
@@ -1,11 +1,5 @@
 versions = [
     {
-        "product": "3.8.3",
-        "java-base": "11",
-        "java-devel": "11",
-        "jmx_exporter": "0.20.0",
-    },
-    {
         "product": "3.8.4",
         "java-base": "11",
         "java-devel": "11",
