c47f29fd27e985f85519d9d99623e82d61a338f8
Benedikt Labrenz <benedikt@labrenz.org>
2024-07-03 11:11:52 +0200

    bump git sync to 4.2.3

--- airflow/versions.py
+++ airflow/versions.py
@@ -2,7 +2,7 @@ versions = [
     {
         "product": "2.6.3",
         "python": "3.9",
-        "git_sync": "v4.2.1",
+        "git_sync": "v4.2.3",
         "statsd_exporter": "0.26.1",
         "tini": "0.19.0",
         "vector": "0.39.0",
@@ -10,7 +10,7 @@ versions = [
     {
         "product": "2.7.2",
         "python": "3.9",
-        "git_sync": "v4.2.1",
+        "git_sync": "v4.2.3",
         "statsd_exporter": "0.26.1",
         "tini": "0.19.0",
         "vector": "0.39.0",
@@ -18,7 +18,7 @@ versions = [
     {
         "product": "2.7.3",
         "python": "3.9",
-        "git_sync": "v4.2.1",
+        "git_sync": "v4.2.3",
         "statsd_exporter": "0.26.1",
         "tini": "0.19.0",
         "vector": "0.39.0",
@@ -26,7 +26,7 @@ versions = [
     {
         "product": "2.8.1",
         "python": "3.9",
-        "git_sync": "v4.2.1",
+        "git_sync": "v4.2.3",
         "statsd_exporter": "0.26.1",
         "tini": "0.19.0",
         "vector": "0.39.0",
@@ -34,7 +34,7 @@ versions = [
     {
         "product": "2.8.3",
         "python": "3.9",
-        "git_sync": "v4.2.1",
+        "git_sync": "v4.2.3",
         "statsd_exporter": "0.26.1",
         "tini": "0.19.0",
         "vector": "0.39.0",
