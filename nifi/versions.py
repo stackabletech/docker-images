b1aed3fbbb69feae573a465764fd07fc9690dc3c
Malte Sander <malte.sander.it@gmail.com>
2024-07-05 09:13:22 +0200

    fix comment

--- nifi/versions.py
+++ nifi/versions.py
@@ -12,7 +12,7 @@ versions = [
     {
         "product": "1.26.0",
         "java-base": "11",
-        "java-devel": "11", # There is an error when trying to use java-devel 21 (for nifi 1.25.0):
+        "java-devel": "11", # There is an error when trying to use java-devel 21 (for nifi 1.26.0):
     },
     {
         "product": "2.0.0-M4",
