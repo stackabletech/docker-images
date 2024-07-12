cdfb7b8e0c338269d97c782205e165d978db55bc
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-07-12 09:06:16 +0200

    Use tag for the opa authorizer

--- hbase/versions.py
+++ hbase/versions.py
@@ -40,7 +40,7 @@ versions = [
         "hbase_profile": "2.6",
         "hadoop": "3.3.6",
         "jmx_exporter": "",  # 2.6 exports jmx and prometheus metrics by default
-        "opa_authorizer": "initial-setup",  # only for HBase 2.6.0
+        "opa_authorizer": "0.1.0",  # only for HBase 2.6.0
         "delete_caches": "true",
     },
 ]
