5571117c84a3bd38e63374ab692e1f58f0ff0857
Sönke Liebau <soenke.liebau@stackable.tech>
2024-07-17 12:48:12 +0200

    Fixed race condition where build containers were fighting over the .m2 cache, when running in parallel by making the cache name unique per version to be built.
    Removed debug statements.
    
    Changed java version for omid 1.1.2 back to 11.

--- omid/versions.py
+++ omid/versions.py
@@ -13,8 +13,8 @@ versions = [
     },
     {
         "product": "1.1.2",
-        "java-base": "17",
-        "java-devel": "17",
+        "java-base": "11",
+        "java-devel": "11",
         "jmx_exporter": "1.0.1",
     },
 ]
