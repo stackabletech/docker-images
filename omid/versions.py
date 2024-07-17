05c0c6785dd7e7ffea7261de953a6c3e70513dd8
Sönke Liebau <soenke.liebau@stackable.tech>
2024-07-17 12:30:25 +0200

    Removed building of the same omid version with multiple java versions.
    Renamed m2 cache volume.

--- omid/versions.py
+++ omid/versions.py
@@ -6,6 +6,12 @@ versions = [
         "jmx_exporter": "1.0.1",
     },
     {
+        "product": "1.1.1",
+        "java-base": "11",
+        "java-devel": "11",
+        "jmx_exporter": "1.0.1",
+    },
+    {
         "product": "1.1.2",
         "java-base": "17",
         "java-devel": "17",
