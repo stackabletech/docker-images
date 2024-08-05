479b8b03b75793fca75902034c31b9b2889fd192
Nick Larsen <nick.larsen@stackable.tech>
2024-08-05 17:47:26 +0200

    chore(opa): add 0.67.0
    https://github.com/open-policy-agent/opa/releases/tag/v0.67.0

--- opa/versions.py
+++ opa/versions.py
@@ -11,4 +11,10 @@ versions = [
         "bundle_builder_version": "1.1.2",
         "stackable-base": "1.0.0",
     },
+    {
+        "product": "0.67.0",
+        "vector": "0.39.0",
+        "bundle_builder_version": "1.1.2",
+        "stackable-base": "1.0.0",
+    },
 ]
