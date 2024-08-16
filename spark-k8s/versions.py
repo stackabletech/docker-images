6d872c2a3f479e65f9eeb4a657a9d7e5823218b4
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-08-16 16:36:31 +0200

    chore(spark): version 4.0.0-preview

--- spark-k8s/versions.py
+++ spark-k8s/versions.py
@@ -47,4 +47,20 @@ versions = [
         "jmx_exporter": "1.0.1",
         "tini": "0.19.0",
     },
+    {
+        "product": "4.0.0-preview1",
+        "java-base": "17",
+        "java-devel": "17",
+        "python": "3.11",
+        "hadoop_long_version": "3.4.0",
+        "aws_java_sdk_bundle": "2.24.6",
+        "azure_storage": "7.0.1",  # https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure/3.3.4
+        "azure_keyvault_core": "1.0.0",  # https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage/7.0.1
+        "jackson_dataformat_xml": "2.15.2",  # https://mvnrepository.com/artifact/org.apache.spark/spark-core_2.13/3.5.1
+        "stax2_api": "4.2.1",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.15.2
+        "woodstox_core": "6.5.1",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.15.2
+        "vector": "0.40.0",
+        "jmx_exporter": "1.0.1",
+        "tini": "0.19.0",
+    },
 ]
