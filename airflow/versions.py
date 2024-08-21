74e8347b15079b7a5e5415646aae79f7449fa478
Nick Larsen <nick.larsen@stackable.tech>
2024-08-21 11:05:22 +0200

    fix(airflow): add airflow 2.9.3, drop previously deprecated versions

--- airflow/versions.py
+++ airflow/versions.py
@@ -1,30 +1,14 @@
 versions = [
     {
-        "product": "2.6.3",
-        "python": "3.9",
-        "git_sync": "v4.2.4",
-        "statsd_exporter": "0.26.1",
-        "tini": "0.19.0",
-        "vector": "0.40.0",
-    },
-    {
-        "product": "2.8.1",
-        "python": "3.9",
-        "git_sync": "v4.2.4",
-        "statsd_exporter": "0.26.1",
-        "tini": "0.19.0",
-        "vector": "0.40.0",
-    },
-    {
-        "product": "2.8.4",
-        "python": "3.9",
-        "git_sync": "v4.2.4",
-        "statsd_exporter": "0.26.1",
-        "tini": "0.19.0",
-        "vector": "0.40.0",
-    },
-    {
         "product": "2.9.2",
+        "python": "3.9",
+        "git_sync": "v4.2.4",
+        "statsd_exporter": "0.26.1",
+        "tini": "0.19.0",
+        "vector": "0.40.0",
+    },
+    {
+        "product": "2.9.3",
         "python": "3.9",
         "git_sync": "v4.2.4",
         "statsd_exporter": "0.26.1",
