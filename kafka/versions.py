83473da93159199b7a923c5a677f04631af91507
Nick Larsen <nick.larsen@stackable.tech>
2024-08-26 21:55:34 +0200

    chore(kafka): add 3.8.0

--- kafka/versions.py
+++ kafka/versions.py
@@ -8,4 +8,13 @@ versions = [
         "opa_authorizer": "1.5.1",
         "jmx_exporter": "1.0.1",
     },
+    {
+        "product": "3.8.0",
+        "java-base": "21",
+        "java-devel": "21",
+        "scala": "2.13",
+        "kcat": "1.7.0",
+        "opa_authorizer": "1.5.1",
+        "jmx_exporter": "1.0.1",
+    },
 ]
