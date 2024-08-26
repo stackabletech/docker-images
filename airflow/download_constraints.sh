d0fc9366a90c5380071fe471be2b4cf5c499a731
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2024-08-27 00:33:48 +1200

    chore: Bump airflow to 2.9.3 (#809)
    * chore: add missing changelog entry for vector
    
    * fix(airflow): the download_constraints script was stripping the dot from the python version which was inconsistent with what the Dockerfile expects
    
    * fix(airflow): add airflow 2.9.3, drop previously deprecated versions
    
    * fix(airflow): update changelog
    
    * Update CHANGELOG.md
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>
    
    * chore(airflow): remove old constraint files
    
    * empty commit to trigger cla-assistant
    
    ---------
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>

--- airflow/download_constraints.sh
+++ airflow/download_constraints.sh
@@ -4,7 +4,7 @@ AIRFLOW_VERSION=${1:?"Missing version number argument (arg 1)"}
 PYTHON_VERSION=${2:-"3.9"}
 
 URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-FILENAME="constraints-${AIRFLOW_VERSION}-python${PYTHON_VERSION//.}.txt"
+FILENAME="constraints-${AIRFLOW_VERSION}-python${PYTHON_VERSION}.txt"
 
 # Find the directory name of the script so it can be run from outside
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
