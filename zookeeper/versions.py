399cc4e16b76ca0fe374b7fdaf73bf172a088434
Nick Larsen <nick.larsen@stackable.tech>
2024-09-12 14:28:07 +0200

    chore(zookeeper): drop back to java 17 (for ZooKeeper 3.9.2)
    It doesn't build, and I can't figure out why. Left a comment for future updaters.

--- zookeeper/versions.py
+++ zookeeper/versions.py
@@ -1,8 +1,8 @@
 versions = [
     {
         "product": "3.9.2",
-        "java-base": "17",
-        "java-devel": "17",
+        "java-base": "11",  # Builds fail on Java 17, so it can stay as is.
+        "java-devel": "11",  # Builds fail on Java 17, so it can stay as is.
         "jmx_exporter": "1.0.1",
     },
 ]
