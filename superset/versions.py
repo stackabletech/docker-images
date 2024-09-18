c449ae0260d4e48cab646e0d94ee2d08d8a6d496
Siegfried Weber <mail@siegfriedweber.net>
2024-09-18 13:49:09 +0200

    chore(vector): update versions for 24.11 (#867)

--- superset/versions.py
+++ superset/versions.py
@@ -2,7 +2,7 @@ versions = [
     {
         "product": "4.0.2",
         "python": "3.9",
-        "vector": "0.40.1",
+        "vector": "0.41.1",
         "statsd_exporter": "0.27.0",
         "authlib": "1.2.1",  # https://github.com/dpgaspar/Flask-AppBuilder/blob/release/4.4.1/requirements/extra.txt#L7
     },
