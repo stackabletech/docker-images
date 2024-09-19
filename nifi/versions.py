70f0f13e4626328c69ade45b4ce40ffc71126b46
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-09-19 05:45:57 -0400

    Update versions.py

--- nifi/versions.py
+++ nifi/versions.py
@@ -2,7 +2,7 @@ versions = [
     {
         "product": "1.27.0",
         "java-base": "11",
-        "java-devel": "11",  # There is an error when trying to use java-devel 21 (for nifi 1.26.0):
+        "java-devel": "11",  # There is an error when trying to use the jdk 21 (since nifi 1.26.0)
     },
     {
         "product": "2.0.0-M4",
