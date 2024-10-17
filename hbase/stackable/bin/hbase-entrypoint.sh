566123ec4ec607e08a458c21907cb8bd6cc6ad4b
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-10-17 12:58:31 +0200

    hbase-entrypoint.sh: update script args

--- hbase/stackable/bin/hbase-entrypoint.sh
+++ hbase/stackable/bin/hbase-entrypoint.sh
@@ -8,14 +8,19 @@ set -euo pipefail
 # master, regionserver, rest
 HBASE_ROLE_NAME="$1"
 # k8s service name for this role+group combo
+# <svc-name>.<namespace>.svc.cluster.local
 HBASE_ROLE_SERVICE_NAME="$2"
 # 16010 for master, 16020 for regionservers etc.
 HBASE_ROLE_SERVICE_PORT="$3"
 
-# TODO: remove hard coded svc.cluster.local
-HBASE_ROLE_SERVICE_HOST="${HOSTNAME}.${HBASE_ROLE_SERVICE_NAME}.${NAMESPACE}.svc.cluster.local"
+HBASE_ROLE_SERVICE_HOST="${HOSTNAME}.${HBASE_ROLE_SERVICE_NAME}"
 
 REGION_MOVER_OPTS="--regionserverhost ${HBASE_ROLE_SERVICE_HOST}:${HBASE_ROLE_SERVICE_PORT} --operation unload ${REGION_MOVER_OPTS}"
+
+if [ -f /stackable/kerberos/krb5.conf ]; then
+  KERBEROS_REALM=$(grep -oP 'default_realm = \\K.*' /stackable/kerberos/krb5.conf)
+  export KERBEROS_REALM
+fi
 
 prepare_signal_handlers() {
   unset term_child_pid
@@ -26,12 +31,12 @@ prepare_signal_handlers() {
 handle_term_signal() {
   if [ "${term_child_pid}" ]; then
     if [ "regionserver" == "${HBASE_ROLE_NAME}" ]; then
-      echo "Running pre-shutdown command: /stackable/hbase/bin/hbase org.apache.hadoop.hbase.util.RegionMover ${REGION_MOVER_OPTS}"
+      echo "Start pre-shutdown"
       # REGION_MOVER_OPTS is a string that contains multiple arguments and needs to be spliced here
       # therefore disable shellcheck for this line
       # shellcheck disable=SC2086
       /stackable/hbase/bin/hbase org.apache.hadoop.hbase.util.RegionMover ${REGION_MOVER_OPTS}
-      echo "Done pre-shutdown command"
+      echo "Done pre-shutdown"
     fi
     kill -TERM "${term_child_pid}" 2>/dev/null
   else
