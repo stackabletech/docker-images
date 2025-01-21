5a94dc9d935bbbb583ece70c410cc8a6d0c5ae2c
Benedikt Labrenz <benedikt@labrenz.org>
2025-01-21 09:59:45 +0100

    remove supersetopa-integration directory

--- superset-opa-integration/install-posgres.sh
+++
@@ -1,4 +0,0 @@
-helm install --wait superset bitnami/postgresql \
-    --set auth.username=superset \
-    --set auth.password=superset \
-    --set auth.database=superset
--- superset-opa-integration/test.py
+++
@@ -1,4 +0,0 @@
-roles = ["Admin", "Test1", None]
-
-for i in range(0, len(roles)):
-    print(f'{i} te rolle ist {roles[i]}')
