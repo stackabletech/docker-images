5f9204687f1164d6b9737ca98fb0b9da7e937d59
Maxi Wittich <maximilian.wittich@stackable.tech>
2025-01-23 14:21:46 +0100

    Adding 0.67.1 dismissed by accident

--- opa/versions.py
+++ opa/versions.py
@@ -6,7 +6,7 @@ versions = [
         "stackable-base": "1.0.0",
     },
     {
-        "product": "1.0.0",
+        "product": "0.67.1",
         "vector": "0.43.1",
         "bundle_builder_version": "1.1.2",
         "stackable-base": "1.0.0",
