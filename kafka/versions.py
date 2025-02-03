982712d711dbb5810f7312f1c7e83355463d8424
Nick Larsen <nick.larsen@stackable.tech>
2025-02-03 10:38:13 +0100

    feat(kafka): Add 3.9.0, remove 3.7.1

--- kafka/versions.py
+++ kafka/versions.py
@@ -1,20 +1,20 @@
 versions = [
     {
-        "product": "3.7.1",
-        "java-base": "21",
-        "java-devel": "21",
-        "scala": "2.13",
-        "kcat": "1.7.0",
-        "opa_authorizer": "1.5.1",
-        "jmx_exporter": "1.0.1-stackable",
-    },
-    {
         "product": "3.8.0",
         "java-base": "21",
         "java-devel": "21",
         "scala": "2.13",
         "kcat": "1.7.0",
         "opa_authorizer": "1.5.1",
-        "jmx_exporter": "1.0.1-stackable",
+        "jmx_exporter": "1.1.0",
+    },
+    {
+        "product": "3.9.0",
+        "java-base": "21",
+        "java-devel": "21",
+        "scala": "2.13",
+        "kcat": "1.7.0",
+        "opa_authorizer": "1.5.1",
+        "jmx_exporter": "1.1.0",
     },
 ]
