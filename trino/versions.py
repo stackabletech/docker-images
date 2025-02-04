dae3ea2154946f127923df6a5eef03a2c9a2ac6b
Nick Larsen <nick.larsen@stackable.tech>
2025-02-04 14:18:48 +0100

    feat(trino): Add 469

--- trino/versions.py
+++ trino/versions.py
@@ -3,14 +3,21 @@ versions = [
         "product": "451",
         "java-base": "22",
         "java-devel": "22",
-        "jmx_exporter": "1.0.1-stackable",
+        "jmx_exporter": "1.1.0",
         "storage_connector": "451",
     },
     {
         "product": "455",
         "java-base": "22",
         "java-devel": "22",
-        "jmx_exporter": "1.0.1-stackable",
+        "jmx_exporter": "1.1.0",
         "storage_connector": "455",
+    },
+    {
+        "product": "469",
+        "java-base": "23",
+        "java-devel": "23",
+        "jmx_exporter": "1.1.0",
+        "storage_connector": "469",
     },
 ]
