2bd4a5214bc7baf7a287b6a61ac8ca90c75b9ed0
Nick Larsen <nick.larsen@stackable.tech>
2025-02-07 12:27:48 +0100

    chore(trino): Split trino-storage-connector into separate build for easier patching with patchable

---
+++ trino-storage-connector/versions.py
@@ -0,0 +1,14 @@
+versions = [
+    {
+        "product": "451",
+        "java-devel": "22",
+    },
+    {
+        "product": "455",
+        "java-devel": "22",
+    },
+    {
+        "product": "469",
+        "java-devel": "23",
+    },
+]
