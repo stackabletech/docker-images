212a573d0a8ac193cf7c185c63b6fb449e6f6fb6
Siegfried Weber <mail@siegfriedweber.net>
2025-02-12 17:56:41 +0100

    feat(opensearch): Change JDK to version 17

--- opensearch/versions.py
+++ opensearch/versions.py
@@ -1,7 +1,9 @@
 versions = [
     {
         "product": "2.18.0",
-        "java-base": "21",
-        "java-devel": "21",
+        # The performance analyzer works with JDK 17, but not with 21.
+        # https://github.com/opensearch-project/performance-analyzer-rca/issues/545
+        "java-base": "17",
+        "java-devel": "17",
     },
 ]
