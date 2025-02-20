5a7959933aeebd3e91afa80e68a44b58b9bb8ff9
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-02-20 10:21:18 +0100

    pin poetry version and use heredoc syntax

--- superset/versions.py
+++ superset/versions.py
@@ -5,6 +5,7 @@ versions = [
         "vector": "0.43.1",
         "statsd_exporter": "0.28.0",
         "authlib": "1.2.1",  # https://github.com/dpgaspar/Flask-AppBuilder/blob/release/4.4.1/requirements/extra.txt#L7
+        "stackable-base": "1.0.0",
     },
     {
         "product": "4.1.1",
@@ -12,5 +13,6 @@ versions = [
         "vector": "0.43.1",
         "statsd_exporter": "0.28.0",
         "authlib": "1.2.1",  # https://github.com/dpgaspar/Flask-AppBuilder/blob/release/4.5.0/requirements/extra.txt#L7
+        "stackable-base": "1.0.0",
     },
 ]
