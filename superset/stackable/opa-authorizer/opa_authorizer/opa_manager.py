31f7b1e126d22c7ddf694082dfa988f24c912ef8
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-02-23 11:12:32 +0100

    docs and silence some checker errors

--- superset/stackable/opa-authorizer/opa_authorizer/opa_manager.py
+++ superset/stackable/opa-authorizer/opa_authorizer/opa_manager.py
@@ -1,5 +1,8 @@
 """
-Custom security manager for Superset
+Custom security manager for Superset.
+
+Assigns OPA roles to a user. The roles and their permissions must exist in the
+Superset database.
 """
 
 import logging
@@ -9,6 +12,7 @@ from typing import Optional
 import requests
 from cachetools import TTLCache, cachedmethod
 from flask import current_app, g
+from flask_appbuilder import AppBuilder
 from flask_appbuilder.security.sqla.models import Role, User
 from overrides import override
 from sqlalchemy.orm.session import Session
@@ -30,7 +34,8 @@ class OpaResponse:
     roles: list[str]
 
 
-def opa_response_from_json(json: dict) -> OpaResponse:
+def opa_response_from_json(json: dict[str, object]) -> OpaResponse:
+    """Converts a JSON object to an OpaResponse object."""
     if "result" in json:
         if type(json["result"]) is list:
             return OpaResponse(roles=json["result"])
@@ -43,14 +48,14 @@ class OpaSupersetSecurityManager(SupersetSecurityManager):
     Custom security manager that syncs role mappings from Open Policy Agent to Superset.
     """
 
-    AUTH_OPA_CACHE_MAXSIZE_DEFAULT = 1000
-    AUTH_OPA_CACHE_TTL_IN_SEC_DEFAULT = 30
-    AUTH_OPA_REQUEST_URL_DEFAULT = "http://opa:8081/"
-    AUTH_OPA_REQUEST_TIMEOUT_DEFAULT = 10
-    AUTH_OPA_PACKAGE_DEFAULT = "superset"
-    AUTH_OPA_RULE_DEFAULT = "user_roles"
+    AUTH_OPA_CACHE_MAXSIZE_DEFAULT: int = 1000
+    AUTH_OPA_CACHE_TTL_IN_SEC_DEFAULT: int = 30
+    AUTH_OPA_REQUEST_URL_DEFAULT: str = "http://opa:8081/"
+    AUTH_OPA_REQUEST_TIMEOUT_DEFAULT: int = 10
+    AUTH_OPA_PACKAGE_DEFAULT: str = "superset"
+    AUTH_OPA_RULE_DEFAULT: str = "user_roles"
 
-    def __init__(self, appbuilder):
+    def __init__(self, appbuilder: AppBuilder):
         super().__init__(appbuilder)
 
         config = appbuilder.get_app.config
@@ -77,7 +82,7 @@ class OpaSupersetSecurityManager(SupersetSecurityManager):
             "AUTH_OPA_REQUEST_TIMEOUT", self.AUTH_OPA_REQUEST_TIMEOUT_DEFAULT
         )
 
-        self.opa_session = requests.Session()
+        self.opa_session: requests.Session = requests.Session()
 
     @override
     def get_user_roles(self, user: Optional[User] = None) -> list[Role]:
@@ -90,24 +95,44 @@ class OpaSupersetSecurityManager(SupersetSecurityManager):
         if not user:
             user = g.user
 
-        resolved_opa_roles = self.roles(user)
+        if user:
+            resolved_opa_roles = self.roles(user)
 
-        self.merge_user_roles(user, resolved_opa_roles)
+            self.merge_user_roles(user, resolved_opa_roles)
 
-        return resolved_opa_roles
+            return resolved_opa_roles
+        else:
+            raise Exception("Cannot get roles without a user.")
 
     @cachedmethod(lambda self: self.role_cache)
     def roles(self, user: User) -> list[Role]:
+        """
+        Retrieves a user's role names from an Open Policy Agent instance.
+        Maps these names to existing Role objects in the Superset database and
+        possibly updates the user entity.
+        The result is cached.
+        """
         opa_role_names = self.opa_get_user_roles(user.username)
-        result = self.resolve_user_roles(user, opa_role_names)
+        result: list[Role] = self.resolve_user_roles(user, opa_role_names)
         return result
 
     def merge_user_roles(self, user: User, roles: list[Role]):
-        if roles:
+        """
+        Updates the roles of a user in the Superset database if neededd.
+        """
+        if self.superset_roles_outdated(user.roles, roles):
             user.roles = roles
+            # We need to use the same SQLA Session that was used to create the object
             sqla_session = Session.object_session(user)
             sqla_session.merge(user)
             sqla_session.commit()
+
+    def superset_roles_outdated(
+        self, superset_roles: list[Role], opa_roles: list[Role]
+    ) -> bool:
+        superset_role_set: set[str] = set([role.name for role in superset_roles])
+        opa_role_set: set[str] = set([role.name for role in opa_roles])
+        return superset_role_set != opa_role_set
 
     def opa_get_user_roles(self, username: str) -> list[str]:
         """
@@ -144,6 +169,16 @@ class OpaSupersetSecurityManager(SupersetSecurityManager):
         )
 
     def resolve_user_roles(self, user: User, roles: list[str]) -> list[Role]:
+        """
+        Given a user object and a list of OPA role names, return the Role objects
+        that must be assigned to this user.
+
+        The user object is only needed to ensure that the Role objects are resolved
+        using the same SQLAlchemy session as the user object.
+
+        The Session object assigned to the SecurityManager is apparently not the same
+        Session as the one used by the FAB login.
+        """
         result: list[Role] = list()
         sqla_session = Session.object_session(user)
         superset_roles = sqla_session.query(Role).all()
