bc4abeea4c20c07b56fac2b910f1f02da85c860e
Siegfried Weber <mail@siegfriedweber.net>
2025-03-31 09:28:08 +0200

    feat(opensearch): Upgrade to version 2.19.1 and install the plugin repository-s3

--- opensearch/versions.py
+++ opensearch/versions.py
@@ -1,6 +1,6 @@
 versions = [
     {
-        "product": "2.19.0",
+        "product": "2.19.1",
         # The performance analyzer works with JDK 17, but not with 21.
         # https://github.com/opensearch-project/performance-analyzer-rca/issues/545
         "java-devel": "17",
