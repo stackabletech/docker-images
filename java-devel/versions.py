0fe577d384017b4e32ab736bf88dcc1341517138
Lukas Krug <lukas.voetmand@stackable.tech>
2025-04-24 15:58:52 +0200

    feat: move patch apply logic to patchable (#1032)
    * wip
    
    * Update druid/Dockerfile
    
    Co-authored-by: Natalie Klestrup Röijezon <nat@nullable.se>
    
    * fix: remove unnecessary check / shadow repo root var
    
    * fix: druid src path
    
    * fix: druid src path
    
    * feat: introduce stackable-devel image
    
    * fix: use PathBuf in ProductVersionContext
    
    * chore: align zookeeper patch directory structure
    
    * fix: stackable-devel dnf and shell config
    
    * chore: switch patch process in other products
    
    * fix: hive build
    
    * fix: trino build
    
    * fix: spark build
    
    * chore: make hadolint happy
    
    * fix: remove hbase intermediate sources / remove unnecessary and operator
    
    * fix: permissions in patchable build process
    
    * chore: remove unnecessary curl command in build process
    
    * chore: move adding of JMX config and start-metastore script from builder stage to final stage
    
    * chore: remove git repo in trino and hbase-operator-tools to avoid maven commit plugin bug
    
    ---------
    
    Co-authored-by: Natalie Klestrup Röijezon <nat@nullable.se>

--- java-devel/versions.py
+++ java-devel/versions.py
@@ -1,26 +1,26 @@
 versions = [
     {
         "product": "8",
-        "stackable-base": "1.0.0",
+        "stackable-devel": "1.0.0",
     },
     {
         "product": "11",
-        "stackable-base": "1.0.0",
+        "stackable-devel": "1.0.0",
     },
     {
         "product": "17",
-        "stackable-base": "1.0.0",
+        "stackable-devel": "1.0.0",
     },
     {
         "product": "21",
-        "stackable-base": "1.0.0",
+        "stackable-devel": "1.0.0",
     },
     {
         "product": "22",
-        "stackable-base": "1.0.0",
+        "stackable-devel": "1.0.0",
     },
     {
         "product": "23",
-        "stackable-base": "1.0.0",
+        "stackable-devel": "1.0.0",
     },
 ]
