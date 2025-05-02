24c042213c7709efe92620bc5fb9460bc36da3e8
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2025-05-02 09:03:03 +0200

    Bump to 0.0.2

--- nifi/versions.py
+++ nifi/versions.py
@@ -13,6 +13,6 @@ versions = [
         "product": "2.2.0",
         "java-base": "21",
         "java-devel": "21",
-        "nifi_iceberg_bundle": "0.0.1",
+        "nifi_iceberg_bundle": "0.0.2",
     },
 ]
