1c05905b5219a96203e8115a54a46631147abcbc
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2025-05-05 21:15:04 +0200

    chore(vector): Bump to 0.46.1 (#1098)
    * chore(vector): Bump to 0.46.1
    
    * chore(vector): Bump products to use 0.46.1

--- java-base/versions.py
+++ java-base/versions.py
@@ -1,27 +1,27 @@
 versions = [
     {
         "product": "8",
-        "vector": "0.43.1",
+        "vector": "0.46.1",
     },
     {
         "product": "11",
-        "vector": "0.43.1",
+        "vector": "0.46.1",
     },
     {
         "product": "17",
-        "vector": "0.43.1",
+        "vector": "0.46.1",
     },
     {
         "product": "21",
-        "vector": "0.43.1",
+        "vector": "0.46.1",
     },
     {
         "product": "22",
-        "vector": "0.43.1",
+        "vector": "0.46.1",
     },
     {
         "product": "23",
-        "vector": "0.43.1",
+        "vector": "0.46.1",
     },
     {
         "product": "24",
