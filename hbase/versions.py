b33909fad6632ac1d8eeb94e013dcce215d31ef0
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-05-06 11:52:39 +0200

    remove hbase 2.4.18

--- hbase/versions.py
+++ hbase/versions.py
@@ -2,20 +2,6 @@ versions = [
     # Also do not merge java-base with java below as "JAVA-BASE is not a valid identifier" in Dockerfiles, it's unfortunate but to fix this would require a bigger refactoring of names or the image tools
     # hbase-thirdparty is used to build the hbase-operator-tools and should be set to the version defined in the POM of HBase.
     {
-        "product": "2.4.18",
-        "hbase_thirdparty": "4.1.5",
-        "hbase_operator_tools": "1.2.0",
-        "java-base": "11",
-        "java-devel": "11",
-        "async_profiler": "2.9",
-        "phoenix": "5.2.1",
-        "hbase_profile": "2.4",
-        "hadoop": "3.3.6",
-        "jmx_exporter": "1.2.0",  # update the stackable/jmx/config<version> folder too
-        "opa_authorizer": "",  # only for HBase 2.6.1
-        "delete_caches": "true",
-    },
-    {
         "product": "2.6.1",
         "hbase_thirdparty": "4.1.9",
         "hbase_operator_tools": "1.3.0-fd5a5fb",
