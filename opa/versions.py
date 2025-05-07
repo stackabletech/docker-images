52f4431f013060ef6818e5e4c68eed94f2f53d60
Nick Larsen <nick.larsen@stackable.tech>
2025-05-07 12:18:35 +0200

    chore(opa): Parameterise golang container version

--- opa/versions.py
+++ opa/versions.py
@@ -2,11 +2,13 @@ versions = [
     {
         "product": "1.4.2",
         "vector": "0.46.1",
+        "golang": "1.23.9",
         "stackable-base": "1.0.0",
     },
     {
         "product": "1.0.1",
         "vector": "0.46.1",
+        "golang": "1.23.9",
         "stackable-base": "1.0.0",
     },
 ]
