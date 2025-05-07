3499ae4d34d512fa76570cf4e617b31f2deae546
Nick Larsen <nick.larsen@stackable.tech>
2025-05-07 11:25:15 +0200

    chore(opa): Add 1.4.2

--- opa/versions.py
+++ opa/versions.py
@@ -1,5 +1,10 @@
 versions = [
     {
+        "product": "1.4.2",
+        "vector": "0.46.1",
+        "stackable-base": "1.0.0",
+    },
+    {
         "product": "1.0.1",
         "vector": "0.46.1",
         "stackable-base": "1.0.0",
