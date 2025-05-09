2491d45bee9f57aac2ce5646840cd0d168908c95
Nick Larsen <nick.larsen@stackable.tech>
2025-05-09 09:22:05 +0200

    chore(druid): Remove 30.0.0

--- druid/versions.py
+++ druid/versions.py
@@ -1,12 +1,5 @@
 versions = [
     {
-        "product": "30.0.0",
-        # https://druid.apache.org/docs/30.0.0/operations/java/
-        "java-base": "17",
-        "java-devel": "17",
-        "authorizer": "0.6.0",
-    },
-    {
         "product": "30.0.1",
         # https://druid.apache.org/docs/30.0.1/operations/java/
         "java-base": "17",
