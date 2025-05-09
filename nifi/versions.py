f1d05ffbaa7b0dbfa996c2b705dc83389714af7c
Benedikt Labrenz <benedikt@labrenz.org>
2025-05-09 14:40:41 +0200

    build nifi opa plugin from tag

--- nifi/versions.py
+++ nifi/versions.py
@@ -3,16 +3,19 @@ versions = [
         "product": "1.27.0",
         "java-base": "11",
         "java-devel": "11",  # There is an error when trying to use the jdk 21 (since nifi 1.26.0)
+        "nifi_opa_authorizer_plugin": "0.1.0",
     },
     {
         "product": "1.28.1",
         "java-base": "11",
         "java-devel": "11",
+        "nifi_opa_authorizer_plugin": "0.1.0",
     },
     {
         "product": "2.2.0",
         "java-base": "21",
         "java-devel": "21",
         "nifi_iceberg_bundle": "0.0.3",
+        "nifi_opa_authorizer_plugin": "0.1.0",
     },
 ]
