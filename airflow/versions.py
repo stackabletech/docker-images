469cd77eeee8682f8016b94d4e620624b223e02a
Andrew Kenworthy <andrew.kenworthy@stackable.tech>
2025-05-15 15:07:57 +0200

    wip

--- airflow/versions.py
+++ airflow/versions.py
@@ -15,4 +15,12 @@ versions = [
         "tini": "0.19.0",
         "vector": "0.43.1",
     },
+    {
+        "product": "3.0.1",
+        "python": "3.12",
+        "git_sync": "v4.4.0",
+        "statsd_exporter": "0.28.0",
+        "tini": "0.19.0",
+        "vector": "0.43.1",
+    },
 ]
