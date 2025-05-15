2a38b9e85af6b0b6842df5bc01d93baf58456ed1
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-05-15 18:19:10 +0200

    add 4.0.0

--- kafka/versions.py
+++ kafka/versions.py
@@ -17,4 +17,13 @@ versions = [
         "opa_authorizer": "1.5.1",
         "jmx_exporter": "1.2.0",
     },
+    {
+        "product": "4.0.0",
+        "java-base": "21",
+        "java-devel": "21",
+        "scala": "2.13",
+        "kcat": "1.7.0",
+        "opa_authorizer": "1.5.1",
+        "jmx_exporter": "1.2.0",
+    },
 ]
