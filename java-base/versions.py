efbc87dcf3fcb897e6e1714929dcc7f758da35d6
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-06-06 11:51:39 +0200

    chore: bump vector version to 0.47.0 (#1152)
    * chore: bump vector version to 0.47.0
    
    * bump vector dependency version
    
    * update changelog

--- java-base/versions.py
+++ java-base/versions.py
@@ -1,30 +1,30 @@
 versions = [
     {
         "product": "8",
-        "vector": "0.46.1",
+        "vector": "0.47.0",
     },
     {
         "product": "11",
-        "vector": "0.46.1",
+        "vector": "0.47.0",
     },
     {
         "product": "17",
-        "vector": "0.46.1",
+        "vector": "0.47.0",
     },
     {
         "product": "21",
-        "vector": "0.46.1",
+        "vector": "0.47.0",
     },
     {
         "product": "22",
-        "vector": "0.46.1",
+        "vector": "0.47.0",
     },
     {
         "product": "23",
-        "vector": "0.46.1",
+        "vector": "0.47.0",
     },
     {
         "product": "24",
-        "vector": "0.46.1",
+        "vector": "0.47.0",
     },
 ]
