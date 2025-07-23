2f15213420b4b9db2624a90734c049ead790a9bd
Benedikt Labrenz <benedikt@labrenz.org>
2025-07-23 11:36:35 +0200

    install performance analyzer plugin

--- opensearch/versions.py
+++ opensearch/versions.py
@@ -4,5 +4,6 @@ versions = [
         "java-devel": "24",
         "jdk-base": "24",
         "opensearch/security-plugin": "3.1.0.0",
+        "opensearch-performance-analyzer": "3.1.0.0",
     },
 ]
