841b3a071df88d30b16cfb16150aeabed6d061c6
Benedikt Labrenz <benedikt@labrenz.org>
2025-07-25 14:36:42 +0200

    build plugins from source and stop building snapshots

--- opensearch/versions.py
+++ opensearch/versions.py
@@ -1,9 +1,9 @@
 versions = [
     {
         "product": "3.1.0",
-        "java-devel": "24",
-        "jdk-base": "24",
+        "java-devel": "21",
+        "jdk-base": "21",
         "opensearch/security-plugin": "3.1.0.0",
-        "opensearch-performance-analyzer": "3.1.0.0",
+        "opensearch/performance-analyzer": "3.1.0.0",
     },
 ]
