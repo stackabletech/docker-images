ba3ccaaa691717f06014cd85ca0524ecf6f4db19
Benedikt Labrenz <benedikt@labrenz.org>
2025-08-07 21:56:40 +0200

    add elasticsearch exporter to opensearch image

--- conf.py
+++ conf.py
@@ -33,6 +33,9 @@ opensearch = importlib.import_module("opensearch.versions")
 opensearch_security_plugin = importlib.import_module(
     "opensearch.security-plugin.versions"
 )
+opensearch_elasticsearch_exporter = importlib.import_module(
+    "opensearch.elasticsearch-exporter.versions"
+)
 spark_k8s = importlib.import_module("spark-k8s.versions")
 stackable_base = importlib.import_module("stackable-base.versions")
 stackable_devel = importlib.import_module("stackable-devel.versions")
@@ -74,6 +77,10 @@ products = [
     {
         "name": "opensearch/security-plugin",
         "versions": opensearch_security_plugin.versions,
+    },
+    {
+        "name": "opensearch/elasticsearch-exporter",
+        "versions": opensearch_elasticsearch_exporter.versions,
     },
     {"name": "spark-k8s", "versions": spark_k8s.versions},
     {"name": "stackable-base", "versions": stackable_base.versions},
---
+++ opensearch/elasticsearch-exporter/versions.py
@@ -0,0 +1,8 @@
+versions = [
+    {
+        "product": "1.9.0",
+        "golang": "1.23.9",
+        "stackable-devel": "1.0.0",
+        "promu": "0.17.0",
+    },
+]
