5f73e7c07bc3f5447a08db073d6d2f26b1d7fde7
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-08-20 14:10:35 +0200

    feat: add spark 4.0.0 image (#1216)
    * feat: add Spark 4.0.0
    
    * update spark client image version
    
    * replace maven-help-plugin with bash-foo
    
    * explicitely tell the build script which maven binary to use
    
    * comment on the hbase connector version

--- spark-k8s/versions.py
+++ spark-k8s/versions.py
@@ -35,4 +35,22 @@ versions = [
         "tini": "0.19.0",
         "hbase_connector": "1.0.1",
     },
+    {
+        "product": "4.0.0",
+        "java-base": "17",
+        "java-devel": "17",
+        "python": "3.11",
+        "hadoop/hadoop": "3.4.1",
+        "hbase": "2.6.2",
+        "aws_java_sdk_bundle": "2.24.6",
+        "azure_storage": "7.0.1",  # https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure/3.3.4
+        "azure_keyvault_core": "1.0.0",  # https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage/7.0.1
+        "jackson_dataformat_xml": "2.15.2",  # https://mvnrepository.com/artifact/org.apache.spark/spark-core_2.13/3.5.1
+        "stax2_api": "4.2.1",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.15.2
+        "woodstox_core": "6.5.1",  # https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-xml/2.15.2
+        "vector": "0.47.0",
+        "jmx_exporter": "1.3.0",
+        "tini": "0.19.0",
+        "hbase_connector": "1.0.1",  # This is not supported in Spark 4 yet.
+    },
 ]
