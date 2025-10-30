494787bc8ea03cc31809b44fe717d48c84125720
Nick Larsen <nick.larsen@stackable.tech>
2025-10-30 11:07:46 +0100

    chore(vector): Build from source (based on ubi9-rust-builder)
    NOTE: The ubi9-rust-builder could not be used as it contains `ONBUILD`
    steps which we need to run after patchable does it's thing. Also it is
    specifically designed for operators and their layout (under `rust/` and
    using workspaces).

--- vector/copy_artifacts.sh
+++ vector/copy_artifacts.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# WARNING: This is currently specific to stackable-operators due to the path to
-# the source (under `rust/`).
+# NOTE: This is a modified version of shared/copy_artifacts.sh
 
 # Copy over the binary
 cp "$1" /app
 
 # And now try to find a BOM file named like the binary + _bin.cdx.xml and copy it over as well if it exists
 base=$(basename "$1")
-find /src/rust/ -type f -name "${base}_bin.cdx.xml" -exec cp {} /app \;
+find /src/ -type f -name "${base}_bin.cdx.xml" -exec cp {} /app \;
