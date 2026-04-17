4b9ee68d1cdabeb42eb46d83e752107efcff1bba
Nick Larsen <nick.larsen@stackable.tech>
2026-04-17 11:42:17 +0200

    fix(vector): Use relative path to patchable checked out source
    Fixes this error:
    
    ```
    find: '/src/': No such file or directory
    ```
    
    Because fot this build, WORKDIR is /stackable (so patchable checked out src/ is below that)

--- vector/copy_artifacts.sh
+++ vector/copy_artifacts.sh
@@ -6,5 +6,7 @@
 cp "$1" /app
 
 # And now try to find a BOM file named like the binary + _bin.cdx.xml and copy it over as well if it exists
+# Using `patchable with --images-repo-root=src` makes the path `/stackable/src/vector/patchable-work/worktree/0.52.0`
+# assuming WORKDIR was `/stackable`.
 base=$(basename "$1")
-find /src/ -type f -name "${base}_bin.cdx.xml" -exec cp {} /app \;
+find ./src/ -type f -name "${base}_bin.cdx.xml" -exec cp {} /app \;
